a674cd6a942ae6fed4bad637e2c73c2b7fcb327f
tnarik <tnarik@lecafeautomatique.co.uk>
2015-04-14 12:44:51 +0100

    Reorganized metadata and added source and issues URLs.

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,17 @@
-name             "bind9-chroot"
-maintainer       "Tnarik Innael"
-maintainer_email "tnarik@lecafeautomatique.co.uk"
-license          "Apache 2.0"
-description      "Installs/Configures bind9 with chroot and hiding CHAOS INFORMATION"
+name             'bind9-chroot'
+maintainer       'Tnarik Innael'
+maintainer_email 'tnarik@lecafeautomatique.co.uk'
+license          'apache2'
+description      'Installs/Configures bind9 with chroot and hiding CHAOS INFORMATION'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.2"
-
-%w{ ubuntu debian centos }.each do |os|
-  supports os
-end
+source_url       'https://github.com/tnarik/chef-bind9-chroot'
+issues_url       'https://github.com/tnarik/chef-bind9-chroot/issues'
+version          '0.4.2'
 
 %w{resolvconf}.each do |cookbook|
   depends(cookbook)
+end
+
+%w{ubuntu debian centos}.each do |os|
+  supports os
 end
